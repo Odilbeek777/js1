4a0c1f50c09b5fc2ab96eb387c8dc08ea5d5295e
odil <odil@gmail.com>
2023-06-20 14:16:06 +0500

    js file

---
+++ script.js
@@ -0,0 +1,27 @@
+var userName = prompt("введите свое имя")
+console.log(userName);
+
+var age = +prompt('введите свой возраст')
+alert(age)
+console.log(age);
+
+var pr1 = +prompt('6+3=?')
+console.log("Пример 1: 6 + 3 = " + (6+3) + " (Ваш ответ " + pr1 +  ") " );
+
+
+var pr2 = +prompt('12-7=?')
+console.log("Пример 2: 12 - 7 = " + (12-7) + " (Ваш ответ " + pr2 +  ") " );
+
+var pr3 = +prompt('5*5=?')
+console.log("Пример 3: 5 * 5 = " + (5*5) + " (Ваш ответ " + pr3 +  ") " );
+
+var pr4 = +prompt('49/7=?')
+console.log("Пример 4: 49 / 7 = " + (49/7) + " (Ваш ответ " + pr4 +  ") " );
+
+var pr5 = +prompt('24%9=?')
+console.log("Пример 5: 24 % 9 = " + (24%9) + " (Ваш ответ " + pr5 +  ") " );
+
+
+var open = prompt('Откройте console')
+alert(open)
+console.log(console);
